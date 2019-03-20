d0bf59b309f5ff4580274710d189ada6c15b958f
srabiller <sebastien.rabiller@linguali.com>
2019-03-20 10:46:01 +0100

    Initial Commit

---
+++ src/com/linguali/Main.java
@@ -0,0 +1,8 @@
+package com.linguali;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ test/com/linguali/BowlingKataTest.java
@@ -0,0 +1,13 @@
+package com.linguali;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingKataTest {
+
+    @Test
+    public void Test(){
+        assertEquals(true, true);
+    }
+}
